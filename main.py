bc1ae902eb09f38bc0e0f90a97c1d4edd2d37d8f
ayman09042001 <156456342+ayman09042001@users.noreply.github.com>
2024-08-02 12:35:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,5 +23,5 @@ async def welcome(name: Name):
     return {f"Request confirmation : Welcome Mister/Miss {name.first_name} {name.last_name}"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8001)
 
